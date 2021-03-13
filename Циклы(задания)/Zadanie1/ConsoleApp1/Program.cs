4ba0247bc8ae47ec650124526eff87a1165c75ca
Moon4man <79942578+Moon4man@users.noreply.github.com>
2021-03-13 12:00:36 +0300

    Add files via upload

---
+++ Циклы(задания)/Zadanie1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pow(double a, int pow)
+            {
+                double result = 1;
+                for (int i = 1; i < pow; i++)
+                {
+                    result *= a;
+                }
+                return result;
+            }
+            double SqrtN(double n, double A, double esp = 0.0001)
+            {
+                var x0 = A / n;
+                var x1 = (1 / n) * ((n - 1) * x0 + A / Pow(x0, (int)n - 1));
+
+                while (Math.Abs(x1 - x0) > esp)
+                {
+                    x0 = x1;
+                    x1 = (1 / n) * ((n - 1) * x0 + A / Pow(x0, (int)n - 1));
+                }
+                return x1;
+            }
+            Console.WriteLine(SqrtN(3, 5));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Циклы(задания)/Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void BinaryCode()
+        {
+            Console.Write("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            string BinaryCode = Convert.ToString(n, 2);
+
+            Console.WriteLine($"Двоичное представление: {BinaryCode}");
+            Console.WriteLine("-----------");
+        }
+
+        static void BinaryCode2()
+        {
+            Console.Write("Введите число: ");
+            int N = Int32.Parse(Console.ReadLine());
+            int a;
+            int i = 0;
+
+            int[] BinaryCode2 = new int[10];
+
+            while (N >= 1)
+            {
+                a = N % 2;
+                BinaryCode2[i] = a;
+                i++;
+
+                N = N / 2;
+            }
+
+            for (i = (BinaryCode2.Length - 1); i >= 0; i--)
+            {
+                Console.Write(BinaryCode2[i]);
+            }
+        }
+        static void Main()
+        {
+            BinaryCode();
+            BinaryCode2();
+            Console.ReadKey();
+        }
+    }
+}
